6146c84acd5102bef955d70d7e1f17a3d3ed5164
pavel <pavel.pereguda@codex-soft.com>
2021-04-08 03:54:17 +0300

    Added 2 modal blocks. Fixed slick slider problem. Added 2 buttons "prev" and "next".

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
-$(function() {
+$(function () {
 
-    $(document).ready(function(){});
+    const apartmentSlider = $('[data-slider="slick"]');
+
+    $(document).ready(function () {
+    });
 
     /* Filter
     =====================*/
     let filter = $("[data-filter]");
 
-    filter.on("click", function(event) {
+    filter.on("click", function (event) {
         event.preventDefault();
 
         let cat = $(this).data('filter');
 
-        if(cat === 'all') {
+        if (cat === 'all') {
             $("[data-cat]").removeClass("hide");
         } else {
-            $("[data-cat]").each(function() {
+            $("[data-cat]").each(function () {
                 let workCat = $(this).data('cat');
 
-                if(workCat !== cat) {
+                if (workCat !== cat) {
                     $(this).addClass('hide');
                 } else {
                     $(this).removeClass('hide');
@@ -32,7 +35,7 @@ $(function() {
     const modalCall = $("[data-modal]");
     const modalClose = $("[data-close]");
 
-    modalCall.on("click", function(event) {
+    modalCall.on("click", function (event) {
         event.preventDefault();
 
         let $this = $(this);
@@ -41,10 +44,10 @@ $(function() {
         $(modalId).addClass('show');
         $("body").addClass('no-scroll');
 
-        $('#apartmentSlider').slick('setPosition');
+        $(apartmentSlider).slick('setPosition');
     });
 
-    modalClose.on("click", function(event) {
+    modalClose.on("click", function (event) {
         event.preventDefault();
 
         let $this = $(this);
@@ -54,21 +57,21 @@ $(function() {
         $("body").removeClass('no-scroll');
     });
 
-    $(".modal").on("click", function() {
+    $(".modal").on("click", function () {
         $(this).removeClass('show');
         $("body").removeClass('no-scroll');
     });
 
-    $(".modal__dialog").on("click", function(event) {
+    $(".modal__dialog").on("click", function (event) {
         event.stopPropagation();
     });
 
 
     /* Scroll
     =====================*/
-    $(function(){
-        $('#scroll_bottom').click(function(){
-            $('html, body').animate({scrollTop: $('#anchor').offset().top }, 600);
+    $(function () {
+        $('#scroll_bottom').click(function () {
+            $('html, body').animate({scrollTop: $('#anchor').offset().top}, 600);
             return false;
         });
     });
@@ -76,13 +79,33 @@ $(function() {
 
     /* Slider: https://kenwheeler.github.io/slick/
     =====================*/
-    $('#apartmentSlider').slick({
+    apartmentSlider.slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
         arrows: false,
         dots: true,
+    });
+
+    $(".order").on("click", function (event) {
+        event.preventDefault();
+    });
+
+    $(".slickPrev").on("click", function(event) {
+        event.preventDefault();
+
+        let currentSlider = $(this).parents('.modal').find(apartmentSlider);
+
+        currentSlider.slick("slickPrev");
+    });
+
+    $(".slickNext").on("click", function(event) {
+        event.preventDefault();
+
+        let currentSlider = $(this).parents('.modal').find(apartmentSlider);
+
+        currentSlider.slick("slickNext");
     });
 
 });
